5fb239d48a5a189e3a0cd048373653685de57580
mohamed abouda <mohamed.aboda500@gmail.com>
2021-02-17 23:56:32 +0200

    first commit

---
+++ client-project/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '44vw3njxk8'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-wo6ji-32.us.auth0.com',
+  clientId: 'g4V6pfESaZCZ0DNeQwQnD8eisYFAlvOs',
+  callbackUrl: 'http://localhost:3000/callback'
+}
